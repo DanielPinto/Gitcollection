c00f8ce6e59c2596a5c5a5011c1b500780ed7893
DanielPinto <danielsp.pinto@gmail.com>
2021-07-22 17:16:04 -0300

    foi configurado o webpackPrefetch e o webpackChunkName nos imports das rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 //import { Dashboard } from '../pages/Dashboard';
 //import { Repo } from '../pages/Repo';
 
-const Dashboard = React.lazy(() => import('../pages/Dashboard'))
-const Repo = React.lazy(() => import('../pages/Repo'))
+const Dashboard = React.lazy(() => import(
+  /*webpackPrefetch: true */
+  /*webpackChunkName: "dashboard" */
+  '../pages/Dashboard'))
+
+const Repo = React.lazy(() => import(
+  /*webpackPrefetch: true */
+  /*webpackChunkName: "repo" */
+  '../pages/Repo'))
 
 export const Routes: React.FC = () => {
   return (
